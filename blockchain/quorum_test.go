3e2f0b2d9556e9ed20fd2fc8c7fab658a4e3b7a0
Fangyu Gai <greferry@gmail.com>
2020-07-19 13:09:27 -0700

    add testing quorum

--- blockchain/quorum_test.go
+++ blockchain/quorum_test.go
@@ -1,7 +1,69 @@
 package blockchain
 
-import "testing"
+import (
+	"testing"
 
-func TestQuorum_SuperMajority(t *testing.T) {
+	"github.com/gitferry/zeitgeber/utils"
+	"github.com/stretchr/testify/require"
+)
 
+// add only one vote
+func TestQuorum_SuperMajority1(t *testing.T) {
+	quorum := NewQuorum(4)
+	blockID := utils.IdentifierFixture()
+	v1 := MakeVote(1, "1", blockID)
+	quorum.Add(v1)
+	require.False(t, quorum.SuperMajority(blockID))
+}
+
+// add only two votes
+func TestQuorum_SuperMajority2(t *testing.T) {
+	quorum := NewQuorum(4)
+	blockID := utils.IdentifierFixture()
+	v1 := MakeVote(1, "1", blockID)
+	quorum.Add(v1)
+	v2 := MakeVote(1, "2", blockID)
+	quorum.Add(v2)
+	require.False(t, quorum.SuperMajority(blockID))
+}
+
+// add three votes for the same block from different nodes
+func TestQuorum_SuperMajority3(t *testing.T) {
+	quorum := NewQuorum(4)
+	blockID := utils.IdentifierFixture()
+	v1 := MakeVote(1, "1", blockID)
+	quorum.Add(v1)
+	v2 := MakeVote(1, "2", blockID)
+	quorum.Add(v2)
+	v3 := MakeVote(1, "3", blockID)
+	quorum.Add(v3)
+	require.True(t, quorum.SuperMajority(blockID))
+}
+
+// add three votes, two for the same block from different nodes
+// one for another block
+func TestQuorum_SuperMajority4(t *testing.T) {
+	quorum := NewQuorum(4)
+	blockID := utils.IdentifierFixture()
+	v1 := MakeVote(1, "1", blockID)
+	quorum.Add(v1)
+	v2 := MakeVote(1, "2", blockID)
+	quorum.Add(v2)
+	v3 := MakeVote(1, "3", utils.IdentifierFixture())
+	quorum.Add(v3)
+	require.False(t, quorum.SuperMajority(blockID))
+}
+
+// add three votes, two for the same block from different nodes
+// one from the same voter
+func TestQuorum_SuperMajority5(t *testing.T) {
+	quorum := NewQuorum(4)
+	blockID := utils.IdentifierFixture()
+	v1 := MakeVote(1, "1", blockID)
+	quorum.Add(v1)
+	v2 := MakeVote(1, "2", blockID)
+	quorum.Add(v2)
+	v3 := MakeVote(1, "2", blockID)
+	quorum.Add(v3)
+	require.False(t, quorum.SuperMajority(blockID))
 }
