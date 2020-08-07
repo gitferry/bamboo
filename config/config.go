ec3a9f0b85fc4ca763bd759a0babfd34d5a76d01
Fangyu Gai <greferry@gmail.com>
2020-08-06 22:11:17 -0700

    add delay 1 16 result

--- config/config.go
+++ config/config.go
@@ -138,7 +138,7 @@ func (c *Config) Load() {
 	}
 
 	// test
-	for i := 1; i <= 4; i++ {
+	for i := 1; i <= 16; i++ {
 		id := identity.NewNodeID(i)
 		port := strconv.Itoa(3000 + i)
 		addr := "tcp://127.0.0.1:" + port
@@ -148,8 +148,8 @@ func (c *Config) Load() {
 		c.HTTPAddrs[id] = addrHttp
 	}
 
-	c.ByzNo = 1
-	c.Threshold = 3
+	c.ByzNo = 2
+	c.Threshold = 11
 
 	c.n = len(c.Addrs)
 	//c.npz = make(map[int]int)
