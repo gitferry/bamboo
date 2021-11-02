10f4c3eace2235654eb14d0f30191fff4479aff5
yangclement <clementyang96@gmail.com>
2021-11-02 01:42:22 -0700

    change based on pull request. remove the implementation for the unused function removeMicroblock, findMicroblock and checkExistence

--- mempool/naivemem.go
+++ mempool/naivemem.go
@@ -1,7 +1,6 @@
 package mempool
 
 import (
-	"bytes"
 	"container/list"
 	"errors"
 	"github.com/gitferry/bamboo/blockchain"
@@ -42,19 +41,28 @@ func (nm *NaiveMem) AddTxn(txn *message.Transaction) (bool, *blockchain.MicroBlo
 	// get the size of the structure. txn is the pointer.
 	tranSize := unsafe.Sizeof(*txn)
 	totalSize := int(tranSize) + nm.currSize
+
 	if totalSize > nm.msize {
 		//do not add the curr trans, and generate a microBlock
 		//set the currSize to curr trans, since it is the only one does not add to the microblock
+		var id crypto.Identifier
 		nm.currSize = int(tranSize)
+		nm.txnList.PushBack(txn)
+		return true, blockchain.NewMicroblock(id, nm.makeTxnSlice())
+
 	} else if totalSize == nm.msize {
 		//add the curr trans, and generate a microBlock
+		var id crypto.Identifier
+		allTxn := append(nm.makeTxnSlice(), txn)
 		nm.currSize = 0
+		return true, blockchain.NewMicroblock(id, allTxn)
+
 	} else {
 		//
+		nm.txnList.PushBack(txn)
 		nm.currSize = totalSize
+		return false, nil
 	}
-	var mb *blockchain.MicroBlock
-	return true, mb
 }
 
 // AddMicroblock adds a microblock into a FIFO queue
@@ -87,7 +95,7 @@ func (nm *NaiveMem) GeneratePayload() *blockchain.Payload {
 	microblockList := make([]*blockchain.MicroBlock, batchSize)
 
 	for i := 0; i < batchSize; i++ {
-		mb := nm.microblocks.Front().Value.(*blockchain.MicroBlock)
+		mb := nm.front()
 		microblockList = append(microblockList, mb)
 	}
 
@@ -98,66 +106,20 @@ func (nm *NaiveMem) GeneratePayload() *blockchain.Payload {
 // in the mempool and return missing ones if there's any
 // return true if there's no missing transactions
 func (nm *NaiveMem) CheckExistence(p *blockchain.Proposal) (bool, []crypto.Identifier) {
-	checkList := p.HashList
-	missingList := make([]crypto.Identifier, 0)
-	allExist := true
-
-	nm.mu.Lock()
-	defer nm.mu.Unlock()
-
-	for _, id := range checkList {
-		_, exist := nm.microblockMap[id]
-		if !exist {
-			allExist = true
-			missingList = append(missingList, id)
-		}
-	}
-
-	if allExist {
-		return allExist, nil
-	} else {
-		return allExist, missingList
-	}
+	id := make([]crypto.Identifier, 0)
+	return false, id
 }
 
 // RemoveMicroblock removes reffered microblocks from the mempool
 func (nm *NaiveMem) RemoveMicroblock(id crypto.Identifier) error {
-	if nm.microblockMap == nil || nm.microblocks == nil {
-		return errors.New("The mempool is not initialized yet")
-	}
-
-	var next *list.Element
-	targetByte := crypto.IDToByte(id)
-
-	for e := nm.microblocks.Front(); e != nil; e = next {
-		next = e.Next()
-		currId := e.Value.(*blockchain.MicroBlock).Hash
-		currByte := crypto.IDToByte(currId)
-
-		if bytes.Equal(currByte, targetByte) {
-			nm.microblocks.Remove(e)
-
-			nm.mu.Lock()
-			defer nm.mu.Unlock()
-
-			delete(nm.microblockMap, id)
-		}
-	}
-
-	return errors.New("Cannot find this MicroBlock")
+	var err error
+	return err
 }
 
 // FindMicroblock finds a reffered microblock
 func (nm *NaiveMem) FindMicroblock(id crypto.Identifier) (bool, *blockchain.MicroBlock) {
-	nm.mu.Lock()
-	defer nm.mu.Unlock()
-
-	block, exist := nm.microblockMap[id]
-	if exist {
-		return true, block
-	} else {
-		return false, nil
-	}
+	var mb *blockchain.MicroBlock
+	return false, mb
 }
 
 // FillProposal pulls microblocks from the mempool and build a pending block,
@@ -177,15 +139,27 @@ func (nm *NaiveMem) FillProposal(p *blockchain.Proposal) *blockchain.PendingBloc
 	return blockchain.NewPendingBlock(p, missingBlocks, existingBlocks)
 }
 
-//func (nm *NaiveMem) front() *blockchain.MicroBlock {
-//	if nm.microblocks.Len() == 0 {
-//		return nil
-//	}
-//	ele := nm.microblocks.Front()
-//	val, ok := ele.Value.(*blockchain.MicroBlock)
-//	if !ok {
-//		return nil
-//	}
-//	nm.microblocks.Remove(ele)
-//	return val
-//}
+func (nm *NaiveMem) front() *blockchain.MicroBlock {
+	if nm.microblocks.Len() == 0 {
+		return nil
+	}
+	ele := nm.microblocks.Front()
+	val, ok := ele.Value.(*blockchain.MicroBlock)
+	if !ok {
+		return nil
+	}
+	nm.microblocks.Remove(ele)
+	return val
+}
+
+func (nm *NaiveMem) makeTxnSlice() []*message.Transaction {
+	allTxn := make([]*message.Transaction, 0)
+	var next *list.Element
+	for e := nm.txnList.Front(); e != nil; e = next {
+		txn := e.Value.(*message.Transaction)
+		allTxn = append(allTxn, txn)
+		nm.txnList.Remove(e)
+		next = e.Next()
+	}
+	return allTxn
+}
