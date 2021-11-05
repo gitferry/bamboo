a30e0bff69d045b6fce0c49d5f60831dc2700ee9
yangclement <clementyang96@gmail.com>
2021-11-05 06:40:17 -0700

    Error with the branch. Restart unit test branch

---
+++ mempool/naivemem_test.go
@@ -0,0 +1,23 @@
+package mempool
+
+import "testing"
+
+func TestNaiveMem_AddMicroblock(t *testing.T) {
+
+}
+
+func TestNaiveMem_AddTxn(t *testing.T) {
+
+}
+
+func TestNaiveMem_FillProposal(t *testing.T) {
+
+}
+
+func TestNaiveMem_GeneratePayload(t *testing.T) {
+
+}
+
+func TestNaiveMem_AddMicroblock2(t *testing.T) {
+
+}
